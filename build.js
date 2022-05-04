c49226b6d4d4b5797d5a65e8b30a0bea9bf67f81
KaMeHb-UA <marlock@bassteam.ml>
2022-05-04 15:28:45 +0300

    add dist version of `package.json` to build script

--- build.js
+++ build.js
@@ -1,9 +1,10 @@
 // @ts-check
 import { build, analyzeMetafile } from 'esbuild';
-import { exit } from 'process';
-import { resolve } from 'path';
+import { cwd, exit } from 'process';
+import { relative, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import pkgJson from './package.json' assert { type: 'json' };
+import { writeFile } from 'fs/promises';
 
 const dirname = resolve(fileURLToPath(import.meta.url), '..'),
     { module: entryPoint, distFile } = pkgJson;
@@ -25,9 +26,48 @@ const buildConfig = {
     legalComments: 'none',
 };
 
+const distPackageJson = {
+    private: true,
+    type: 'module',
+};
+
+async function writePkgJson(){
+    const target = resolve(buildConfig.outfile, '..', 'package.json');
+    const pkgJSON = Buffer.from(JSON.stringify(distPackageJson), 'utf8');
+    await writeFile(target, pkgJSON);
+    const from = '[[runtime]]';
+    return {
+        metafile: {
+            inputs: {
+                [from]: {
+                    bytes: 0,
+                    imports: [],
+                },
+            },
+            outputs: {
+                [relative(cwd(), target)]: {
+                    imports: [],
+                    exports: [],
+                    entryPoint: from,
+                    inputs: {
+                        [from]: {
+                            bytesInOutput: pkgJSON.length,
+                        },
+                    },
+                    bytes: pkgJSON.length,
+                },
+            },
+        },
+    };
+}
+
 try{
-    const result = await build(buildConfig);
-    console.log(await analyzeMetafile(result.metafile));
+    const result = build(buildConfig);
+    const distPkgJsonResult = await writePkgJson();
+    const metafile = JSON.parse(JSON.stringify((await result).metafile));
+    Object.assign(metafile.inputs, distPkgJsonResult.metafile.inputs);
+    Object.assign(metafile.outputs, distPkgJsonResult.metafile.outputs);
+    console.log(await analyzeMetafile(metafile));
 } catch(e){
     exit(1);
 }
