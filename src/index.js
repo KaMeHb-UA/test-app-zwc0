36d56f9da547cad8054de827d8ef8db342d072f5
KaMeHb-UA <marlock@bassteam.ml>
2022-05-04 15:11:10 +0300

    add handling for investment variants absence

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import periods from '@/providers/periods';
 import sorter from '@/providers/sorter';
 import balance from '@/providers/balance';
 import runner from '@/runner';
+import { stdout } from 'process';
 
-console.log('Loading gold prices...');
+// with carriage return to rewrite this line when results have been got
+await new Promise(r => stdout.write('Loading gold prices...\r', r));
 
 const { buy, sell, profit, profitPercentage } = await runner({
     prices,
@@ -17,7 +19,11 @@ function round(num){
     return Math.round(num * 100) / 100
 }
 
-console.log(`
-The best moment to buy gold was ${buy} and to sell - ${sell}.
-Having initial credit of ${balance()} PLN you could got ~${round(profit)} PLN more (~${round(profitPercentage)}% of initial).
-`);
+if(buy === undefined){
+    console.log('Sadly, but there was no good moment to invest. Just keep your money. Or buy some coffe :)');
+} else {
+    console.log([
+        `The best moment to buy gold was ${buy} and to sell - ${sell}.`,
+        `Having initial credit of ${balance()} PLN you could got ~${round(profit)} PLN more (~${round(profitPercentage)}% of initial).`
+    ].join('\n'));
+}
